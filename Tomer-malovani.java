dbe06b056054d02eec4d5597edd5c2a3fbc3d3ee
tomermalovani <tomermalovani@gmail.com>
2023-04-20 18:00:09 +0300

    second push

--- Tomer-malovani.java
+++ Tomer-malovani.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TomerMalovani {
+public class Tomer-malovani {
 	public static void main(String[] args){
 		for (int i = 0; i <= 10; i++) {
 			System.out.println("Tomer Malovani...."+i);
