1082b9ff3c8c477b24714d7c4d13acfae66865d6
tomermalovani <tomermalovani@gmail.com>
2023-04-20 17:54:11 +0300

    first push

---
+++ Tomer-malovani.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TomerMalovani {
+	public static void main(String[] args){
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("Tomer Malovani...."+i);
+
+		}
+
+
+
+	
+
+
+		
+}
