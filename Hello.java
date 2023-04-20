20cb40b27b41e3edac49bc45400f214338ce0461
tomermalovani <tomermalovani@gmail.com>
2023-04-20 18:13:08 +0300

    hello world last test hopefully

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Hello{
 	public static void main(String[] args){
 		for (int i = 0; i <= 10; i++) {
-			System.out.println("goodbye World 47...."+i);
+			System.out.println("goodbye World 47 last test?...."+i);
 
 		}
 
